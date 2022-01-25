84f9996490c368da3da9644e96af13e4338676f9
Usman Tarfa <utarfa@gmail.com>
2022-01-25 14:44:19 -0500

    Adding loading indicators

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -5,15 +5,15 @@ import NavBar from './NavBar';
 import ActivityDashBoard from '../../features/activities/dashboard/ActivityDashboard';
 import { v4 as uuid } from 'uuid';
 import agent from '../api/agent';
+import LoadingComponent from './LoadingComponent';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
-
   const [selectedActivity, setSelectedActivity] = useState<
     Activity | undefined
   >(undefined);
-
   const [editMode, setEditMode] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     agent.Activities.list().then((response) => {
@@ -24,6 +24,7 @@ function App() {
       });
       console.log(response);
       setActivities(activities);
+      setLoading(false);
     });
   }, []);
 
@@ -57,6 +58,10 @@ function App() {
 
   function handleDeleteActivity(id: string) {
     setActivities([...activities.filter((x) => x.id !== id)]);
+  }
+
+  if (loading) {
+    return <LoadingComponent content='Loading app' />;
   }
 
   return (
