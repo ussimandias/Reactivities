ea810dc304ea0abd006d7d532672ac35e3984bbb
Usman Tarfa <utarfa@gmail.com>
2022-01-31 23:04:21 -0500

    Creating a details view

---
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Button, Card, Image, Item } from 'semantic-ui-react';
+import { Activity } from '../../../app/models/activity';
+
+interface Props {
+  activity: Activity;
+}
+
+export default function ActivityDetails({ activity }: Props) {
+  return (
+    <Card>
+      <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
+      <Card.Content>
+        <Card.Header>{activity.title}</Card.Header>
+        <Card.Meta>
+          <span>{activity.date}</span>
+        </Card.Meta>
+        <Card.Description>{activity.description}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Button.Group width='2'>
+          <Button basic color='blue' content='Edit' />
+          <Button basic color='grey' content='Cancel' />
+        </Button.Group>
+      </Card.Content>
+    </Card>
+  );
+}
