2d831d6d83057408e955968a480fef55de69f62e
Usman Tarfa <utarfa@gmail.com>
2022-02-02 16:39:46 -0500

    Setting up axios

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import Navbar from './NavBar';
 import ActivityDashBoard from '../../features/activities/dashboard/ActivitiesDashboard';
 import { v4 as uuid } from 'uuid';
+import agent from '../api/agent';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -14,12 +14,10 @@ function App() {
   const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
-    axios
-      .get<Activity[]>('http://localhost:5000/api/activities')
-      .then((response) => {
-        console.log(response);
-        setActivities(response.data);
-      });
+    agent.Activities.list().then((response) => {
+      console.log(response);
+      setActivities(response);
+    });
   }, []);
 
   function handleSelectActivity(id: string) {
