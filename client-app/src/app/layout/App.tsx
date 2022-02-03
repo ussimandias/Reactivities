ca108529a878295170b3ad2c0eea98533ce4b6d0
Usman Tarfa <utarfa@gmail.com>
2022-02-02 21:23:23 -0500

    Setting up MobX

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -6,8 +6,11 @@ import ActivityDashBoard from '../../features/activities/dashboard/ActivitiesDas
 import { v4 as uuid } from 'uuid';
 import agent from '../api/agent';
 import LoadingComponent from './LoadingComponent';
+import { useStore } from '../stores/store';
 
 function App() {
+  const { activityStore } = useStore();
+
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<
     Activity | undefined
@@ -82,6 +85,8 @@ function App() {
     <>
       <Navbar openForm={handleFormOpen} />
       <Container style={{ marginTop: '7em' }}>
+        <h2>{activityStore.title}</h2>
+
         <ActivityDashBoard
           activities={activities}
           selectedActivity={selectedActivity}
