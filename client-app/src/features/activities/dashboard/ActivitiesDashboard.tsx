d3b7a7027e164584f33d038a0158c163d6edb618
Usman Tarfa <utarfa@gmail.com>
2022-02-02 19:26:20 -0500

    Deleting activity on the server

--- client-app/src/features/activities/dashboard/ActivitiesDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivitiesDashboard.tsx
@@ -37,6 +37,7 @@ export default function ActivityDashBoard({
           activities={activities}
           selectActivity={selectActivity}
           deleteActivity={deleteActivity}
+          submitting={submitting}
         />
       </Grid.Column>
       <Grid.Column width='6'>
