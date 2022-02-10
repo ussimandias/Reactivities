35e5b76158bb66cddbb74b593bcb33684d141171
Usman Tarfa <utarfa@gmail.com>
2022-02-10 16:43:15 -0500

    Adding a key to the route

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -4,11 +4,12 @@ import Navbar from './NavBar';
 import ActivityDashBoard from '../../features/activities/dashboard/ActivitiesDashboard';
 import { observer } from 'mobx-react-lite';
 import HomePage from '../../features/home/HomePage';
-import { Route } from 'react-router-dom';
+import { Route, useLocation } from 'react-router-dom';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
 
 function App() {
+  const location = useLocation();
   return (
     <>
       <Navbar />
@@ -17,6 +18,7 @@ function App() {
         <Route exact path='/activities' component={ActivityDashBoard} />
         <Route path='/activities/:id' component={ActivityDetails} />
         <Route
+          key={location.key}
           path={['/createActivity', '/manage/:id']}
           component={ActivityForm}
         />
