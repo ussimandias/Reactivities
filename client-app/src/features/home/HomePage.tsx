56cf450d490623304e20d462c76e722160237852
Usman Tarfa <utarfa@gmail.com>
2022-02-11 16:24:25 -0500

    Adding the activity filter component

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,13 +1,16 @@
 import { Link } from 'react-router-dom';
-import { Container } from 'semantic-ui-react';
+import { Button, Container, Header, Image, Segment } from 'semantic-ui-react';
 
 export default function HomePage() {
   return (
-    <Container style={{ marginTop: '7em' }}>
-      <h1>Home page</h1>
-      <h3>
-        Go To <Link to='/activities'>Activities</Link>
-      </h3>
-    </Container>
+    <Segment>
+      <Container>
+        <Header>
+          <Image />
+          <Header />
+          <Button></Button>
+        </Header>
+      </Container>
+    </Segment>
   );
 }
