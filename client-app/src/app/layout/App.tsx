bea38bddbbde927f27ce1940fae02de96150a0b0
Usman Tarfa <utarfa@gmail.com>
2022-02-14 22:37:24 -0500

    Preparing for setting up error handling in the client app

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -6,11 +6,14 @@ import HomePage from '../../features/home/HomePage';
 import { Route, useLocation } from 'react-router-dom';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
+import TestErrors from '../../features/errors/TestError';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   const location = useLocation();
   return (
     <>
+      <ToastContainer position='bottom-right' hideProgressBar />
       <Route exact path='/' component={HomePage} />
       <Route
         path={'/(.+)'}
@@ -25,6 +28,7 @@ function App() {
                 path={['/createActivity', '/manage/:id']}
                 component={ActivityForm}
               />
+              <Route path={'/errors'} component={TestErrors} />
             </Container>
           </>
         )}
