01c8ccfb4be7c3979c1e7f94176107362122b1c3
Usman Tarfa <utarfa@gmail.com>
2022-02-15 16:42:29 -0500

    Adding a not found component

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,11 +3,12 @@ import Navbar from './NavBar';
 import ActivityDashBoard from '../../features/activities/dashboard/ActivitiesDashboard';
 import { observer } from 'mobx-react-lite';
 import HomePage from '../../features/home/HomePage';
-import { Route, useLocation } from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
 import TestErrors from '../../features/errors/TestError';
 import { ToastContainer } from 'react-toastify';
+import NotFound from '../../features/errors/NotFound';
 
 function App() {
   const location = useLocation();
@@ -21,14 +22,17 @@ function App() {
           <>
             <Navbar />
             <Container style={{ marginTop: '7em' }}>
-              <Route exact path='/activities' component={ActivityDashBoard} />
-              <Route path='/activities/:id' component={ActivityDetails} />
-              <Route
-                key={location.key}
-                path={['/createActivity', '/manage/:id']}
-                component={ActivityForm}
-              />
-              <Route path={'/errors'} component={TestErrors} />
+              <Switch>
+                <Route exact path='/activities' component={ActivityDashBoard} />
+                <Route path='/activities/:id' component={ActivityDetails} />
+                <Route
+                  key={location.key}
+                  path={['/createActivity', '/manage/:id']}
+                  component={ActivityForm}
+                />
+                <Route path={'/errors'} component={TestErrors} />
+                <Route component={NotFound} />
+              </Switch>
             </Container>
           </>
         )}
--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -2,7 +2,7 @@ import { observer } from 'mobx-react-lite';
 import { Fragment } from 'react';
 import { Header, Item, Segment } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
-import ActivityListItem from './ActivityListItem';
+import ActivityListItem from './ActivityListitem';
 
 export default observer(function ActivityList() {
   const { activityStore } = useStore();
