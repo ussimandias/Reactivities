75a34f9b8d0860bc92b78fd6ced16806a52c9876
Usman Tarfa <utarfa@gmail.com>
2022-02-16 10:51:53 -0500

    Validation in Formik

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,11 +1,12 @@
 import { observer } from 'mobx-react-lite';
 import React, { ChangeEvent, useEffect, useState } from 'react';
 import { Link, useHistory, useParams } from 'react-router-dom';
-import { Button, Segment } from 'semantic-ui-react';
+import { Button, FormField, Label, Segment } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { useStore } from '../../../app/stores/store';
 import { v4 as uuid } from 'uuid';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 export default observer(function ActivityForm() {
   const history = useHistory();
@@ -28,6 +29,10 @@ export default observer(function ActivityForm() {
     date: '',
     city: '',
     venue: '',
+  });
+
+  const validationSchema = Yup.object({
+    title: Yup.string().required('The activity title is required'),
   });
 
   useEffect(() => {
@@ -61,13 +66,20 @@ export default observer(function ActivityForm() {
   return (
     <Segment clearing>
       <Formik
+        validationSchema={validationSchema}
         enableReinitialize
         initialValues={activity}
         onSubmit={(values) => console.log(values)}
       >
         {({ handleSubmit }) => (
           <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
-            <Field placeholder='Title' name='title' />
+            <FormField>
+              <Field placeholder='Title' name='title' />
+              <ErrorMessage
+                name='title'
+                render={(error) => <Label basic color='red' content={error} />}
+              />
+            </FormField>
             <Field placeholder='Description' name='description' />
             <Field placeholder='Category' name='category' />
             <Field type='date' placeholder='Date' name='date' />
