a36807cc96da251881809068a75a1cc17859df96
Usman Tarfa <utarfa@gmail.com>
2022-02-17 22:13:16 -0500

    Creating the user DTOs

---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,9 @@
+
+
+public class RegisterDto
+{
+    public string DisplayName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string UserName { get; set; }
+}
---
+++ API/DTOs/UserDto.cs
@@ -0,0 +1,7 @@
+public class UserDto
+{
+    public string DisplayName { get; set; } 
+    public string Token { get; set; }
+    public string Username { get; set; }
+    public string  Image { get; set; }
+}
