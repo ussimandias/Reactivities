7857b564aaccdc82e6ac97ac94e329ec504db97c
Usman Tarfa <utarfa@gmail.com>
2022-02-17 21:48:21 -0500

    Configuring identity in the Program.cs

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Identity;
+using Persistence;
+
+public static class IdentityServiceExtentions
+{
+    public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddIdentityCore<AppUser>( opt =>
+        {
+            opt.Password.RequireNonAlphanumeric = false;
+
+        })
+        .AddEntityFrameworkStores<DataContext>()
+        .AddSignInManager<SignInManager<AppUser>>();
+        
+        services.AddAuthentication();
+
+        return services;
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -12,6 +12,9 @@ builder.Services.AddControllers().AddFluentValidation(config =>{
 
 });
 
+//builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
