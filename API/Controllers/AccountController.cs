8b2b87e1fe3bcd7cc069e1e99ed2a3993f9b9c42
Usman Tarfa <utarfa@gmail.com>
2022-02-18 18:57:20 -0500

    Registering new users

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 [AllowAnonymous]
 [ApiController]
@@ -38,5 +39,40 @@ public class AccountController : ControllerBase
         }
 
         return Unauthorized();
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+    {
+        if(await userManager.Users.AnyAsync(x => x.Email == registerDto.Email))
+        {
+            return BadRequest("Email taken");
+        }
+        if(await userManager.Users.AnyAsync(x => x.UserName == registerDto.UserName))
+        {
+            return BadRequest("Username taken");
+        }
+
+        var user = new AppUser
+        {
+            DisplayName = registerDto.DisplayName,
+            Email = registerDto.Email,
+            UserName = registerDto.UserName
+        };
+
+        var result = await userManager.CreateAsync(user, registerDto.Password);
+
+        if(result.Succeeded)
+        {
+            return new UserDto
+            {
+                 DisplayName = registerDto.DisplayName,
+                 Image = null,
+                 Token = tokenService.CreateToken(user),
+                 Username = user.UserName
+            };
+        }
+
+        return BadRequest("Problem registering user");
     }
 }
