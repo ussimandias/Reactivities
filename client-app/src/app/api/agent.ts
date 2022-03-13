7314feea88767b0bd95124ba66b2dc3ac68af838
Usman Tarfa <utarfa@gmail.com>
2022-03-12 21:19:15 -0500

    Creating a user store

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -75,15 +75,10 @@ const Activities = {
 };
 
 const Account = {
-  current: () => {
-    requests.get<User>('/account');
-  },
-  login: (user: UserFormValues) => {
-    requests.post<User>('/account/login', user);
-  },
-  register: (user: UserFormValues) => {
-    requests.post<User>('/account/register', user);
-  },
+  current: () => requests.get<User>('/account'),
+  login: (user: UserFormValues) => requests.post<User>('/account/login', user),
+  register: (user: UserFormValues) =>
+    requests.post<User>('/account/register', user),
 };
 
 const agent = {
