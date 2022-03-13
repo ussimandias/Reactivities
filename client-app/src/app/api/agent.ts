40ec878e9921b7eba4c91fafd7da5def15eb2c4b
Usman Tarfa <utarfa@gmail.com>
2022-03-12 20:42:08 -0500

    Creating the interfaces and methods

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -2,6 +2,7 @@ import axios, { AxiosError, AxiosResponse } from 'axios';
 import { toast } from 'react-toastify';
 import { history } from '../..';
 import { Activity } from '../models/activity';
+import { User, UserFormValues } from '../models/user';
 import { store } from '../stores/store';
 
 const sleep = (delay: number) => {
@@ -73,8 +74,21 @@ const Activities = {
   delete: (id: string) => requests.del<void>(`/activities/${id}`),
 };
 
+const Account = {
+  current: () => {
+    requests.get<User>('/account');
+  },
+  login: (user: UserFormValues) => {
+    requests.post<User>('/account/login', user);
+  },
+  register: (user: UserFormValues) => {
+    requests.post<User>('/account/register', user);
+  },
+};
+
 const agent = {
   Activities,
+  Account,
 };
 
 export default agent;
