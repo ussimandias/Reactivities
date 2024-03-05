f12f5d7f2c9e5a119dc0ed2bf07fddc92e45ae42
Usman Tarfa <utarfa@gmail.com>
2024-03-04 21:24:31 -0500

    setting up the web.config for production

--- Application/Core/Result.cs
+++ Application/Core/Result.cs
@@ -6,3 +6,4 @@ public class Result<T>
     public static Result<T> Success(T value) => new Result<T> {IsSuccess = true, Value = value};
     public static Result<T> Failure(string error) => new Result<T> {IsSuccess = false, Error = error};
 }
+
